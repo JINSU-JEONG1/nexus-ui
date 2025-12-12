59d3cb9e0bde73bb72f0d2804ce8a0fbddc85277
Jinsu_Jeong <104358180+JINSU-JEONG1@users.noreply.github.com>
2025-12-12 23:46:06 +0900

    feat(header) : side bar 디자인변경

--- src/components/navigation/NavHeader.vue
+++ src/components/navigation/NavHeader.vue
@@ -3,13 +3,13 @@
     <div class="header-content">
       <button 
         class="menu-toggle" 
+        :class="{ 'menu-toggle--active': isSidebarOpen }"
         @click="$emit('toggle-sidebar')"
         type="button"
-        :title="isSidebarOpen ? '사이드바 닫기' : '사이드바 열기'"
+        :title="isSidebarOpen ? 'closeSidebar' : 'openSidebar'"
       >
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path v-if="isSidebarOpen" d="M4 6L16 6M4 10L16 10M4 14L16 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-          <path v-else d="M6 6L14 14M14 6L6 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+          <path d="M4 6L16 6M4 10L16 10M4 14L16 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
         </svg>
       </button>
       <div class="header-title">
